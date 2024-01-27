a4206a8d7b1d67548edeed938ea3ec3415aa52ff
Jacob <58263528+mykasero@users.noreply.github.com>
2024-01-27 23:00:27 +0100

    Update wordy_TODO.py

--- Chapters/String_methods/wordy_TODO.py
+++ Chapters/String_methods/wordy_TODO.py
@@ -1,16 +1,28 @@
 def answer(question):
     answer = 0
+    numbers = []
+    operations = 0
+    minus_numbers = 0
     # "?" is annoying, remove it
     
     question = question.replace("?","")
-    numbers = []
+    
+    
     err_counter = 0
     #chop the question into words
     
     words = question.split()
+    # checking the amount of negative numbers
 
+    for word in range(len(words)-1):
+        if word < 0:
+            minus_numbers+=1
+
+    
     # for an operation keyword, do the math for the item index before and after the keyword
-    # for only for single operation keyword
+    
+    # for only for single operation keyword (works but doesnt raise a good syntax error for missing operation)
+    
     if len(words) < 4:
         answer = int(words[len(words)-1])
     elif words[0] != "What":
@@ -18,17 +30,42 @@ def answer(question):
     else:
         for word in range(len(words)-1):
             if words[word] == "plus":
-                answer = int(words[word-1]) + int(words[word+1])
+                if operations > 0 :
+                    answer += int(words[word+1])
+                else:
+                    answer += int(words[word-1]) + int(words[word+1])
+                    operations+=1
             elif words[word] == "minus":
-                answer = int(words[word-1]) - int(words[word+1])
+                if int(words[word+1]) < 0 and operations > 0:
+                    answer += abs(int(words[word+1]))
+                elif int(words[word+1]) < 0 and operations == 0:
+                    answer = int(words[word-1]) + abs(int(words[word+1]))
+                    operations+=1
+                elif int(words[word+1]) >= 0 and operations == 0:
+                    answer = int(words[word-1]) - int(words[word+1])
+                    operations+=1
+                else:
+                    answer -= int(words[word+1])
             elif words[word] == "multiplied":
-                answer = int(words[word-1]) * int(words[word+2])
+                if operations > 0:
+                    answer *= int(words[word+2])
+                else:
+                    answer = int(words[word-1]) * int(words[word+2])
+                    operations+=1
             elif words[word] == "divided":
-                answer = int(words[word-1]) / int(words[word+2])
+                if operations > 0 and minus_numbers%2 == 0:
+                    answer /= abs(int(words[word-1])) / abs(int(words[word+2]))
+                elif operations == 0 and minus_numbers%2 == 0:
+                    answer = abs(int(words[word-1])) / abs(int(words[word+2]))
+                    operations+=1
+                elif operations == 0 and minus_numbers%2 != 0 :
+                    answer = int(words[word-1]) / int(words[word+2])
+                    operations+=1
+                else:
+                    answer /= int(words[word+2])
             elif words[word] not in ["plus","minus","multiplied","divided"]:
                 err_counter +=1
-        if len(words)-1 != err_counter:
-            raise ValueError("unknown operation")
+
 
 
     return answer
