982dbcf4bc7afa4c43a781304c0165f404a79c21
Jacob <58263528+mykasero@users.noreply.github.com>
2024-02-11 21:48:15 +0100

    Add files via upload

--- Chapters/Tuples/twelve_days.py
+++ Chapters/Tuples/twelve_days.py
@@ -1,25 +1,26 @@
 def recite(start_verse, end_verse):
     base_text_1 = "On the "
     base_text_2 = " day of Christmas my true love gave to me: "
-    
+
     all_gifts = ('and a Partridge in a Pear Tree.', 'two Turtle Doves, ', 'three French Hens, ',
-             'four Calling Birds, ', 'five Gold Rings, ', 'six Geese-a-Laying, ', 'seven Swans-a-Swimming, ',
-             'eight Maids-a-Milking, ', 'nine Ladies Dancing, ', 'ten Lords-a-Leaping, ', 'eleven Pipers Piping, ',
-             'twelve Drummers Drumming ')
-    
-    days = ("first","second","third","fourth","fifth","sixth","seventh","eighth","ninth","tenth","eleventh","twelfth")
+                 'four Calling Birds, ', 'five Gold Rings, ', 'six Geese-a-Laying, ', 'seven Swans-a-Swimming, ',
+                 'eight Maids-a-Milking, ', 'nine Ladies Dancing, ', 'ten Lords-a-Leaping, ', 'eleven Pipers Piping, ',
+                 'twelve Drummers Drumming, ')
+
+    days = ("first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh",
+            "twelfth")
 
     final = []
-    loop_iter = end_verse-1
-    
-    if end_verse == 1 and start_verse==1:
-        day_string = base_text_1 + days[end_verse - 1] + base_text_2
-        final.append(day_string + all_gifts[0].replace("and ", ""))
+
+    if end_verse == 1 and start_verse == 1:
+        final.append(base_text_1 + days[0] + base_text_2 + all_gifts[0].replace("and ", ""))
     else:
-        for day_num in range(start_verse, end_verse+1):
-            day_string = base_text_1 + days[day_num - 1] + base_text_2
-            gifts = all_gifts[day_num - 1::-1]  # Get the corresponding gifts for this day
-            final.append(day_string + ''.join(gifts))
-            
-    
+        for iter1 in range(start_verse, end_verse + 1):
+            if iter1 == 1:
+                final.append(base_text_1 + days[0] + base_text_2 + all_gifts[0].replace("and ", ""))
+            else:
+                final.append(base_text_1 + days[iter1 - 1] + base_text_2 + "".join(all_gifts[iter1 - 1::-1]))
+
     return final
+
+
