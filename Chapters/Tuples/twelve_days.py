ec0ce2d28dc9edf8873fb7d59108cf373fd5f87c
Jacob <58263528+mykasero@users.noreply.github.com>
2024-02-11 01:38:23 +0100

    Update twelve_days.py

--- Chapters/Tuples/twelve_days.py
+++ Chapters/Tuples/twelve_days.py
@@ -1,31 +1,25 @@
 def recite(start_verse, end_verse):
-    base_text = "On the first day of Christmas my true love gave to me:"
-    all_gifts = ['and a Partridge in a Pear Tree.', 'two Turtle Doves,', 'three French Hens,',
-                 'four Calling Birds,', 'five Gold Rings,', 'six Geese-a-Laying,', 'seven Swans-a-Swimming,',
-                 'eight Maids-a-Milking,', 'nine Ladies Dancing,', 'ten Lords-a-Leaping,', 'eleven Pipers Piping,',
-                 'twelve Drummers Drumming']
+    base_text_1 = "On the "
+    base_text_2 = " day of Christmas my true love gave to me: "
+    
+    all_gifts = ('and a Partridge in a Pear Tree.', 'two Turtle Doves, ', 'three French Hens, ',
+             'four Calling Birds, ', 'five Gold Rings, ', 'six Geese-a-Laying, ', 'seven Swans-a-Swimming, ',
+             'eight Maids-a-Milking, ', 'nine Ladies Dancing, ', 'ten Lords-a-Leaping, ', 'eleven Pipers Piping, ',
+             'twelve Drummers Drumming ')
+    
+    days = ("first","second","third","fourth","fifth","sixth","seventh","eighth","ninth","tenth","eleventh","twelfth")
 
-    # gift_list = all_gifts.split(",")
-    gift_list2 = []
-
-    for iter in range(len(all_gifts) - 1, 0, -1):
-        gift_list2.append(all_gifts[iter])
-
-    # print(gift_list,'\n',gift_list2)
-    strings = [gift_list2[0].strip()]
-
-    gift_list2.remove(gift_list2[0])
-
-    final = base_text
-    for item in gift_list2:
-        # print(item)
-        item += ","
-        strings.append(item.strip())
-
-    if end_verse == 1:
-        final.join()
+    final = []
+    loop_iter = end_verse-1
+    
+    if end_verse == 1 and start_verse==1:
+        day_string = base_text_1 + days[end_verse - 1] + base_text_2
+        final.append(day_string + all_gifts[0].replace("and ", ""))
     else:
-        for iter in range(end_verse - 1, 0, -1):
-            final += '\n' + strings[iter]
-
+        for day_num in range(start_verse, end_verse+1):
+            day_string = base_text_1 + days[day_num - 1] + base_text_2
+            gifts = all_gifts[day_num - 1::-1]  # Get the corresponding gifts for this day
+            final.append(day_string + ''.join(gifts))
+            
+    
     return final
