0495cc4c0631d5779ada11ed0c13d98133c71a6c
Jacob <58263528+mykasero@users.noreply.github.com>
2024-02-15 21:28:44 +0100

    Update square_root.py

--- Chapters/Numbers/square_root.py
+++ Chapters/Numbers/square_root.py
@@ -1,3 +1,19 @@
+'''
+Instructions
+Given a natural radicand, return its square root.
+
+Note that the term "radicand" refers to the number for which the root is to be determined. That is, it is the number under the root symbol.
+
+Check out the Wikipedia pages on square root and methods of computing square roots.
+
+Recall also that natural numbers are positive real whole numbers (i.e. 1, 2, 3 and up).
+
+How this Exercise is Structured in Python
+Python offers a wealth of mathematical functions in the form of the math module and built-ins such as pow() and sum(). However, we'd like you to consider the challenge of solving this exercise without those built-ins or modules.
+
+While there is a mathematical formula that will find the square root of any number, we have gone the route of having only natural numbers (positive integers) as solutions.
+'''
+
 def square_root(number):
     return_val = 0
     val = number
