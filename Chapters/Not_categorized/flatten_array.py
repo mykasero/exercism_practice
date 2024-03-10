6366fb7817025a183de07af0e573468d153ee14f
Jacob <58263528+mykasero@users.noreply.github.com>
2024-03-10 22:44:39 +0100

    Update flatten_array.py

--- Chapters/Not_categorized/flatten_array.py
+++ Chapters/Not_categorized/flatten_array.py
@@ -8,3 +8,18 @@ def flatten(iterable):
             final.append(item)
 
     return final
+
+'''
+Instructions
+Take a nested list and return a single flattened list with all values except nil/null.
+
+The challenge is to write a function that accepts an arbitrarily-deep nested list-like structure and returns a flattened structure without any nil/null values.
+
+For example:
+
+input: [1,[2,3,null,4],[null],5]
+
+output: [1,2,3,4,5]
+'''
+# These tests are auto-generated with test data from:
+# https://github.com/exercism/problem-specifications/tree/main/exercises/flatten-array/canonical-data.json
