9d5b5a29af013c93633472717122387a936c3d07
Jacob <58263528+mykasero@users.noreply.github.com>
2024-03-13 20:37:02 +0100

    Update pig_latin.py

--- Chapters/Conditionals/pig_latin.py
+++ Chapters/Conditionals/pig_latin.py
@@ -1,17 +1,11 @@
 def translate(text):
-    # vowel - samogloska
     vowels = ['a', 'e', 'u', 'i', 'o']
-
-    # consonant - spolgloska
+    
     consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y',
                   'z']
 
     word = text
     iter = 0
-    #for i in range(len(text) - 1):
-    #    if text[i] == 'y':
-    #        break
-    #    iter += 1
 
     if " " in text:
         text = text.split()
@@ -38,3 +32,18 @@ def translate(text):
         word = text[2:] + text[:2] + 'ay'
 
     return word
+
+'''
+Instructions
+Implement a program that translates from English to Pig Latin.
+
+Pig Latin is a made-up children's language that's intended to be confusing. It obeys a few simple rules (below), but when it's spoken quickly it's really difficult for non-children (and non-native speakers) to understand.
+
+Rule 1: If a word begins with a vowel sound, add an "ay" sound to the end of the word. Please note that "xr" and "yt" at the beginning of a word make vowel sounds (e.g. "xray" -> "xrayay", "yttria" -> "yttriaay").
+Rule 2: If a word begins with a consonant sound, move it to the end of the word and then add an "ay" sound to the end of the word. Consonant sounds can be made up of multiple consonants, such as the "ch" in "chair" or "st" in "stand" (e.g. "chair" -> "airchay").
+Rule 3: If a word starts with a consonant sound followed by "qu", move it to the end of the word, and then add an "ay" sound to the end of the word (e.g. "square" -> "aresquay").
+Rule 4: If a word contains a "y" after a consonant cluster or as the second letter in a two letter word it makes a vowel sound (e.g. "rhythm" -> "ythmrhay", "my" -> "ymay").
+There are a few more rules for edge cases, and there are regional variants too. Check the tests for all the details.
+'''
+# These tests are auto-generated with test data from:
+# https://github.com/exercism/problem-specifications/tree/main/exercises/pig-latin/canonical-data.json
