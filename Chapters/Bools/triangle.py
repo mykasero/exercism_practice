3701e7399c6b47fb220a33c7e4e651f73a13f649
Jacob <58263528+mykasero@users.noreply.github.com>
2024-03-13 20:33:38 +0100

    Update triangle.py

--- Chapters/Bools/triangle.py
+++ Chapters/Bools/triangle.py
@@ -1,3 +1,46 @@
+def equilateral(sides):
+    a = sides[0]
+    b = sides[1]
+    c = sides[2]
+    is_true = False
+
+    if a == b == c and a != 0:
+        is_true = True
+
+    return is_true
+
+
+def isosceles(sides):
+    is_true = False
+    a = sides[0]
+    b = sides[1]
+    c = sides[2]
+
+    if a == b and a != c or b == c and b != a or c == a and c != b or a == b == c and a != 0:
+        is_true = True
+    if a + b < c or b + c < a or c + a < b:
+        is_true = False
+
+    return is_true
+
+
+def scalene(sides):
+    is_true = False
+    a = sides[0]
+    b = sides[1]
+    c = sides[2]
+
+    if a != 0 and b != 0 and c != 0 and a != b != c:
+        is_true = True
+
+    if a == b or b == c or a == c:
+        is_true = False
+
+    if a + b < c or b + c < a or c + a < b:
+        is_true = False
+
+    return is_true
+
 '''
 Instructions
 Determine if a triangle is equilateral, isosceles, or scalene.
@@ -19,49 +62,5 @@ a + b ≥ c
 b + c ≥ a
 a + c ≥ b
 '''
-
-def equilateral(sides):
-    # rownoboczny
-    a = sides[0]
-    b = sides[1]
-    c = sides[2]
-    is_true = False
-
-    if a == b == c and a != 0:
-        is_true = True
-
-    return is_true
-
-
-def isosceles(sides):
-    # rownoramienny
-    is_true = False
-    a = sides[0]
-    b = sides[1]
-    c = sides[2]
-
-    if a == b and a != c or b == c and b != a or c == a and c != b or a == b == c and a != 0:
-        is_true = True
-    if a + b < c or b + c < a or c + a < b:
-        is_true = False
-
-    return is_true
-
-
-def scalene(sides):
-    # rozboczny
-    is_true = False
-    a = sides[0]
-    b = sides[1]
-    c = sides[2]
-
-    if a != 0 and b != 0 and c != 0 and a != b != c:
-        is_true = True
-
-    if a == b or b == c or a == c:
-        is_true = False
-
-    if a + b < c or b + c < a or c + a < b:
-        is_true = False
-
-    return is_true
+# These tests are auto-generated with test data from:
+# https://github.com/exercism/problem-specifications/tree/main/exercises/triangle/canonical-data.json
