0a30527a8252fad2c50848a5dbca6302d10b5bc5
Jacob <58263528+mykasero@users.noreply.github.com>
2024-03-14 21:06:59 +0100

    Update matching_brackets.py

--- Chapters/Conditionals/matching_brackets.py
+++ Chapters/Conditionals/matching_brackets.py
@@ -19,3 +19,11 @@ def is_paired(input_string):
 
     else:
         return False
+'''
+Instructions
+Given a string containing brackets [], braces {}, parentheses (), or any combination thereof, 
+verify that any and all pairs are matched and nested correctly. 
+The string may also contain other characters, which for the purposes of this exercise should be ignored.
+'''
+# These tests are auto-generated with test data from:
+# https://github.com/exercism/problem-specifications/tree/main/exercises/matching-brackets/canonical-data.json
