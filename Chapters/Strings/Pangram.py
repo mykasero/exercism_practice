d27f863e6ffb2b3c798a9fbfac00411c619ccf8d
Jacob <58263528+mykasero@users.noreply.github.com>
2024-03-14 21:07:46 +0100

    Update Pangram.py

--- Chapters/Strings/Pangram.py
+++ Chapters/Strings/Pangram.py
@@ -18,4 +18,14 @@ def is_pangram(sentence):
         is_true = True
     
     return is_true
-    
+
+'''
+Instructions
+Your task is to figure out if a sentence is a pangram.
+
+A pangram is a sentence using every letter of the alphabet at least once. It is case insensitive, so it doesn't matter if a letter is lower-case (e.g. k) or upper-case (e.g. K).
+
+For this exercise, a sentence is a pangram if it contains each of the 26 letters in the English alphabet.
+'''
+# These tests are auto-generated with test data from:
+# https://github.com/exercism/problem-specifications/tree/main/exercises/pangram/canonical-data.json
