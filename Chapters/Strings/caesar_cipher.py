a9e93b6c6b6df140c12c4e000bb0ee16159ec27b
Jacob <58263528+mykasero@users.noreply.github.com>
2024-03-15 23:29:13 +0100

    Update caesar_cipher.py

--- Chapters/Strings/caesar_cipher.py
+++ Chapters/Strings/caesar_cipher.py
@@ -48,3 +48,22 @@ def rotate(text, key):
                     ciphered += letters_num[dict_num]
         counter += 1
     return ciphered
+
+'''
+Instructions
+Create an implementation of the rotational cipher, also sometimes called the Caesar cipher.
+
+The Caesar cipher is a simple shift cipher that relies on transposing all the letters in the alphabet using an integer key between 0 and 26. Using a key of 0 or 26 will always yield the same output due to modular arithmetic. The letter is shifted for as many values as the value of the key.
+
+The general notation for rotational ciphers is ROT + <key>. The most commonly used rotational cipher is ROT13.
+
+A ROT13 on the Latin alphabet would be as follows:
+
+Plain:  abcdefghijklmnopqrstuvwxyz
+Cipher: nopqrstuvwxyzabcdefghijklm
+It is stronger than the Atbash cipher because it has 27 possible keys, and 25 usable keys.
+
+Ciphertext is written out in the same formatting as the input including spaces and punctuation.
+'''
+# These tests are auto-generated with test data from:
+# https://github.com/exercism/problem-specifications/tree/main/exercises/rotational-cipher/canonical-data.json
