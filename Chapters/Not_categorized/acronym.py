1083c5131033ce63096a13baa12b425c8034e1c5
Jacob <58263528+mykasero@users.noreply.github.com>
2024-03-16 18:02:53 +0100

    Update acronym.py

--- Chapters/Not_categorized/acronym.py
+++ Chapters/Not_categorized/acronym.py
@@ -1,7 +1,30 @@
 def abbreviate(words):
     final = ""
+    
     words = words.replace("-"," ").replace(",","").replace("_","")
     w_split = words.split()
+    
     for item in w_split:
         final += item[0].upper()
+    
     return final
+
+'''
+Instructions
+Convert a phrase to its acronym.
+
+Techies love their TLA (Three Letter Acronyms)!
+
+Help generate some jargon by writing a program that converts a long name like Portable Network Graphics to its acronym (PNG).
+
+Punctuation is handled as follows: hyphens are word separators (like whitespace); all other punctuation can be removed from the input.
+
+For example:
+
+Input	                    Output
+As Soon As Possible	        ASAP
+Liquid-crystal display	    LCD
+Thank George It's Friday!	TGIF
+'''
+# These tests are auto-generated with test data from:
+# https://github.com/exercism/problem-specifications/tree/main/exercises/acronym/canonical-data.json
