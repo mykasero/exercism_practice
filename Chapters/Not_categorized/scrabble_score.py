3a2262bf44e6c83c93df74391e8cd637e79f5aaf
Jacob <58263528+mykasero@users.noreply.github.com>
2024-03-18 23:28:54 +0100

    Update scrabble_score.py

--- Chapters/Not_categorized/scrabble_score.py
+++ Chapters/Not_categorized/scrabble_score.py
@@ -8,3 +8,39 @@ def score(word):
         score += let_scores[letter.upper()]
 
     return score
+
+'''
+Instructions
+Given a word, compute the Scrabble score for that word.
+
+Letter Values
+You'll need these:
+
+Letter                           Value
+A, E, I, O, U, L, N, R, S, T       1
+D, G                               2
+B, C, M, P                         3
+F, H, V, W, Y                      4
+K                                  5
+J, X                               8
+Q, Z                               10
+Examples
+"cabbage" should be scored as worth 14 points:
+
+3 points for C
+1 point for A, twice
+3 points for B, twice
+2 points for G
+1 point for E
+And to total:
+
+3 + 2*1 + 2*3 + 2 + 1
+= 3 + 2 + 6 + 3
+= 5 + 9
+= 14
+Extensions
+You can play a double or a triple letter.
+You can play a double or a triple word.
+'''
+# These tests are auto-generated with test data from:
+# https://github.com/exercism/problem-specifications/tree/main/exercises/scrabble-score/canonical-data.json
