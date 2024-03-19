cdfbb2fa6b9fbe7e458618d057127eb09e89a12e
Jacob <58263528+mykasero@users.noreply.github.com>
2024-03-19 20:49:37 +0100

    Update prime_factors.py

--- Chapters/Not_categorized/prime_factors.py
+++ Chapters/Not_categorized/prime_factors.py
@@ -13,3 +13,18 @@ def factors(value):
     return divisors
 
 print(factors(1257642))
+
+'''
+Instructions
+Compute the prime factors of a given natural number.
+
+A prime number is only evenly divisible by itself and 1.
+
+Note that 1 is not a prime number.
+
+Example
+What are the prime factors of 60?
+Our successful divisors in that computation represent the list of prime factors of 60: 2, 2, 3, and 5.
+'''
+# These tests are auto-generated with test data from:
+# https://github.com/exercism/problem-specifications/tree/main/exercises/prime-factors/canonical-data.json
