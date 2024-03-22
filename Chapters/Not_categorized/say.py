a9dfe6cbec9ca9f8d41b17434d883fc45a4465f7
Jacob <58263528+mykasero@users.noreply.github.com>
2024-03-22 22:06:29 +0100

    Add files via upload

--- Chapters/Not_categorized/say.py
+++ Chapters/Not_categorized/say.py
@@ -1,63 +1,105 @@
-def say(number):
+singles = {0: "zero", 1: "one", 2: "two", 3: "three",
+           4: "four", 5: "five", 6: "six", 7: "seven",
+           8: "eight", 9: "nine"}
+teens = {10: "ten", 11: "eleven", 12: "twelve",
+         13: "thirteen", 14: "fourteen", 15: "fifteen",
+         16: "sixteen", 17: "seventeen", 18: "eighteen",
+         19: "nineteen"}
+tens = {0: "", 2: "twenty", 3: "thirty", 4: "forty",
+        5: "fifty", 6: "sixty", 7: "seventy",
+        8: "eighty", 9: "ninety"}
+
+
+def less_100(number):
     final = ""
-    txt_num = ""
 
-    num_to_word_1 = {0 : "zero", 1 : "one", 2 : "two", 3 : "three",
-                   4 : "four", 5 : "five", 6 : "six", 7 : "seven",
-                   8 : "eight", 9 : "nine"}
-    num_to_word_2 = {10 : "ten", 11 : "eleven", 12 : "twelve",
-                     13 : "thirteen", 14 : "fourteen", 15 : "fifteen",
-                     16 : "sixteen", 17 : "seventeen", 18 : "eighteen",
-                     19 : "nineteen"}
-    num_to_word_3 = {2 : "twenty", 3 : "thirty", 4 : "forty",
-                     5 : "fifty", 6 : "sixty", 7 : "seventy",
-                     8 : "eighty", 9 : "ninety"}
-    words = []
-    numbers = []
-    num_rev = str(number)
-    print(num_rev)
-
-    if len(num_rev) > 12 or number < 0:
+    # step 1
+    if number < 0:
         raise ValueError("input out of range")
+    elif number < 100:
+        if number < 10:
+            final += singles[number]
+        elif number >= 10 and number < 20:
+            final += teens[number]
+        else:
+            if str(number)[1] == "0":
+                final += tens[int(str(number)[0])]
+            else:
+                final += tens[int(str(number)[0])] + "-" + singles[int(str(number)[1])]
 
-    ctr = 0
-    i = 0
-    while i < len(num_rev):
-        txt_num += num_rev[i]
-        ctr += 1
-        i += 1
-        if ctr == 3:
-            numbers.append(txt_num)
-            txt_num = ""
-            ctr = 0
-    numbers = numbers[::-1]
-    for i in range(len(numbers)):
-        for j in range(len(numbers[i])):
-            #print(numbers[i][j])
-            if j == 0:
-                txt_num += num_to_word_1[int(numbers[i][j])] + " hundred "
-                # print(num_to_word_1[int(numbers[i][j])],"hundred")
-            elif j == 1:
-                if numbers[i][j] == "1" and j == 1:
-                    txt_num += num_to_word_2[int(numbers[i][1:])]
-                    # print(num_to_word_2[int(numbers[i][1:])])
-                else:
-                    txt_num += num_to_word_3[int(numbers[i][j])]
-                    # print(num_to_word_3[int(numbers[i][j])])
-            elif j == 2 and numbers[i][1] != "1":
-                txt_num += "-" + num_to_word_1[int(numbers[i][j])]
-                # print(num_to_word_1[int(numbers[i][j])])
-        if i == 1:
-            words.append(" thousand ")
-        elif i == 2:
-            words.append(" million ")
-        elif i == 3:
-            words.append(" billion ")
-        words.append(txt_num)
-        txt_num = ""
-
-    print(words[::-1])
-    #print(txt_num,numbers)
     return final
 
-print(say(879678345217))
+
+def say(number):
+    final = ""
+
+    num2 = number
+    num_list = []
+
+    words = []
+    words2 = []
+
+    test_elements = []
+
+    if number > 999_999_999_999:
+        raise ValueError("input out of range")
+
+    elif number < 100:
+        final += less_100(number)
+
+    elif number >= 100:
+        while num2 >= 1:
+            num_list.append(num2 % 1000)
+            num2 = int(num2 / 1000)
+
+        print(num_list)
+
+        # step 2-3
+        for i in range(len(num_list)):
+            if i == 0:
+                words.append(num_list[i])
+            elif i == 1:
+                words.append(" thousand ")
+                words.append(int(str(num_list[i])))
+
+            elif i == 2:
+                words.append(" million ")
+                words.append(int(str(num_list[i])))
+
+            elif i == 3:
+                words.append(" billion ")
+                words.append(int(str(num_list[i])))
+
+        # step 4
+        for item in words[::-1]:
+            if len(str(item)) <= 3:
+                if len(str(item)) == 3:
+                    for i in range(len(str(item))):
+
+                        if i == 0 and str(item)[i]:
+                            final += singles[int(str(item)[i])] + " hundred "
+                        if i == 1 and str(item)[1] == "1":
+                            final += teens[int(str(item)[i:i + 1])]
+                        elif i == 1 and str(item)[1] != "1":
+                            if str(item)[1] == "0" and str(item)[2] != "0":
+                                final += singles[int(str(item)[i + 1])]
+                            elif str(item)[1] != "0" and str(item)[2] == "0":
+                                final += tens[int(str(item)[i])]
+                            elif str(item)[1] != "0" and str(item)[2] != "0":
+                                final += tens[int(str(item)[i])] + "-" + singles[int(str(item)[i + 1])]
+                    test_elements = final.split()
+                else:
+                    if item != 0:
+                        final += less_100(item)
+                    test_elements = final.split()
+            else:
+                test_elements = final.split()
+                if test_elements[len(test_elements) - 1] in ["thousand", "million", "billion"]:
+                    pass
+                else:
+                    final += item
+    # remove whitespace at the end of the string
+    if final[-1:] == " ":
+        final = final.rstrip()
+
+    return final
