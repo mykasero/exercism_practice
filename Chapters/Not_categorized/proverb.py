4c6809608620bcea214fc2e69f5130cb56ee95f2
Jacob <58263528+mykasero@users.noreply.github.com>
2024-03-24 22:07:40 +0100

    Update proverb.py

--- Chapters/Not_categorized/proverb.py
+++ Chapters/Not_categorized/proverb.py
@@ -25,3 +25,23 @@ def proverb(*input_data,qualifier):
 
 
     return final
+
+'''
+Instructions
+For want of a horseshoe nail, a kingdom was lost, or so the saying goes.
+
+Given a list of inputs, generate the relevant proverb. For example, given the list ["nail", "shoe", "horse", "rider", "message", "battle", "kingdom"], you will output the full text of this proverbial rhyme:
+
+For want of a nail the shoe was lost.
+For want of a shoe the horse was lost.
+For want of a horse the rider was lost.
+For want of a rider the message was lost.
+For want of a message the battle was lost.
+For want of a battle the kingdom was lost.
+And all for the want of a nail.
+
+How this exercise is implemented for Python
+The test cases for this track add an additional keyword argument called qualifier. You should use this keyword arguments value to modify the final verse of your Proverb.
+'''
+# These tests are auto-generated with test data from:
+# https://github.com/exercism/problem-specifications/tree/main/exercises/proverb/canonical-data.json
