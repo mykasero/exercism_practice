1fe56cfb0775bc85b4d6f88ab70b7da7347c3ecd
Jacob <58263528+mykasero@users.noreply.github.com>
2024-04-20 21:42:26 +0200

    Update crypto_square.py

--- Chapters/Not_categorized/crypto_square.py
+++ Chapters/Not_categorized/crypto_square.py
@@ -73,7 +73,57 @@ def cipher_text(plain_text):
 
     return final
 
+'''
+Instructions
+Implement the classic method for composing secret messages called a square code.
 
-test_text = "If man was meant to stay on the ground, god would have given us roots."
+Given an English text, output the encoded version of that text.
 
-print(cipher_text(test_text))
+First, the input is normalized: the spaces and punctuation are removed from the English text and the message is down-cased.
+
+Then, the normalized characters are broken into rows. These rows can be regarded as forming a rectangle when printed with intervening newlines.
+
+For example, the sentence
+
+"If man was meant to stay on the ground, god would have given us roots."
+is normalized to:
+
+"ifmanwasmeanttostayonthegroundgodwouldhavegivenusroots"
+The plaintext should be organized into a rectangle as square as possible. The size of the rectangle should be decided by the length of the message.
+
+If c is the number of columns and r is the number of rows, then for the rectangle r x c find the smallest possible integer c such that:
+
+r * c >= length of message,
+and c >= r,
+and c - r <= 1.
+Our normalized text is 54 characters long, dictating a rectangle with c = 8 and r = 7:
+
+"ifmanwas"
+"meanttos"
+"tayonthe"
+"groundgo"
+"dwouldha"
+"vegivenu"
+"sroots  "
+The coded message is obtained by reading down the columns going left to right.
+
+The message above is coded as:
+
+"imtgdvsfearwermayoogoanouuiontnnlvtwttddesaohghnsseoau"
+Output the encoded text in chunks that fill perfect rectangles (r X c), with c chunks of r length, separated by spaces. 
+For phrases that are n characters short of the perfect rectangle, pad each of the last n chunks with a single trailing space.
+
+"imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn  sseoau "
+Notice that were we to stack these, we could visually decode the ciphertext back in to the original message:
+
+"imtgdvs"
+"fearwer"
+"mayoogo"
+"anouuio"
+"ntnnlvt"
+"wttddes"
+"aohghn "
+"sseoau "
+'''
+# These tests are auto-generated with test data from:
+# https://github.com/exercism/problem-specifications/tree/main/exercises/crypto-square/canonical-data.json
