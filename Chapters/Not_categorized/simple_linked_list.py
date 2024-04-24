3daa41908f8300584864a55b8420935fa2118be2
Jacob <58263528+mykasero@users.noreply.github.com>
2024-04-24 21:23:16 +0200

    Update simple_linked_list.py

--- Chapters/Not_categorized/simple_linked_list.py
+++ Chapters/Not_categorized/simple_linked_list.py
@@ -53,3 +53,18 @@ class LinkedList:
 class EmptyListException(Exception):
     def __init__(self, msg):
         super().__init__(msg)
+
+'''
+Introduction
+You work for a music streaming company.
+
+You've been tasked with creating a playlist feature for your music player application.
+
+Instructions
+Write a prototype of the music player application.
+
+For the prototype, each song will simply be represented by a number. Given a range of numbers (the song IDs), create a singly linked list.
+
+Given a singly linked list, you should be able to reverse the list to play the songs in the opposite order.
+'''
+#No link to text cases available
