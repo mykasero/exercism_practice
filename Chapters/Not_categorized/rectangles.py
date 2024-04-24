91bd290ee4b18d8de7cd533080f8ba6a9e4f86a3
Jacob <58263528+mykasero@users.noreply.github.com>
2024-04-24 20:17:03 +0200

    Add files via upload

--- Chapters/Not_categorized/rectangles.py
+++ Chapters/Not_categorized/rectangles.py
@@ -1,44 +1,18 @@
-#Needs a re-do, prob best to split into separate functions
-
-def rectangles(strings):
-    final = 0
-
-    ctr_plus = 0
-    ctr_pause = 0
-    ctr_wall = 0
-
-    ctrs = []
-
-    if len(strings) in [0, 1]:
-        return 0
-    else:
-        for string in strings:
-            for item in string:
-                if item == "+":
-                    ctr_plus += 1
-                elif item == "-":
-                    ctr_pause += 1
-                elif item == "|":
-                    ctr_wall += 1
-
-            ctrs.append([ctr_plus, ctr_pause, ctr_wall])
-            ctr_plus = 0
-            ctr_pause = 0
-
-    if ctrs[0][0] - 1 == ctrs[0][1] and len(strings) < 4:
-        final = ctrs[0][1]
-
-    elif ctrs[0][0] > 0 and ctrs[0][1] == 0:
-        for pluses in ctrs:
-            ctr_pause += pluses[1]
-        if ctr_pause == 0:
-            final = 1
-    else:
-        for items in ctrs:
-            ctr_plus += items[0]
-            ctr_pause += items[1]
-        if ctr_plus == ctr_pause:
-            final = 1
-
-    return final
-
+def rectangles(s):
+    rects = 0
+    for y in range(len(s)):
+        for x in range(len(s[0])):
+            if s[y][x] == '+':
+                for yi in range(y+1, len(s)):
+                    if s[yi][x] == "+":
+                        for xi in range(x+1, len(s[0])):
+                            if s[yi][xi] == '+':
+                                if s[y][xi] == '+' and \
+                                    len(s[y][x+1:xi+1].replace('-', '').replace('+', '')) == 0 and \
+                                        all((s[l][xi] in '+|') for l in range(y+1, yi+1)):
+                                    rects += 1
+                            elif s[yi][xi] != '-':
+                                break
+                    elif s[yi][x] != "|":
+                        break
+    return rects
