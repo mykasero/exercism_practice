c4d8660290a763cb5f6aeb5e60de45bd2141ccaa
Jacob <58263528+mykasero@users.noreply.github.com>
2024-04-26 22:55:29 +0200

    Add files via upload

---
+++ Chapters/Not_categorized/connect.py
@@ -0,0 +1,48 @@
+class ConnectGame:
+    def __init__(self, board):
+        self.board = [[stone for stone in row] for row in board.replace(' ', '').splitlines()]  # Generate 2d array
+        self.neighbours = [(0, 1), (1, 0), (1, -1), (0, -1), (-1, 0), (-1, 1)]  # hex neighbours relative pos in array
+        #         movement:   >       \/      \/<       <        /\      /\>
+
+    def get_winner(self):  # check player 0, then X, if neither win output ' '
+        for player in ['O', 'X']:
+            result = self.checker(player)
+            if result is True:
+                return player
+        return ''
+
+    def checker(self, player):
+        if player == 'O':  # set parameters for player O
+            array = self.board
+        if player == 'X':  # set parameters for player X
+            array = list(zip(*self.board))  # transpose the board, so X becomes top to down
+        # get player first and last row stones
+        stones = []
+        ends = []
+
+        for index,stone in enumerate(array[0]):
+            if stone == player:
+                stones.append((index, 0))
+                break
+
+        for index, stone in enumerate(array[-1]):
+            if stone == player:
+                ends.append((index, len(array)-1))
+                break
+
+        # search for stones connected to first row stones, loop till exhaustion
+        for stone in stones:
+            for neighbour in self.neighbours:
+                row = stone[1] + neighbour[1]
+                col = stone[0] + neighbour[0]
+                if row >= 0 and col >= 0:
+                    try:
+                        if array[row][col] == player and (col, row) not in stones:
+                            stones.append((col, row))  # append stone to end of list so it will be looped over
+                    except IndexError:
+                        continue
+
+        # return true if one of the end stones is connected to first row stones
+        for end in ends:
+            if end in stones:
+                return True
