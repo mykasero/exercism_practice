bbd6470003053363037fee6a118cb06aa0374fcf
Jacob <58263528+mykasero@users.noreply.github.com>
2024-04-28 22:20:45 +0200

    Update minesweeper.py

--- Chapters/Lists/minesweeper.py
+++ Chapters/Lists/minesweeper.py
@@ -1,4 +1,4 @@
-# STATUS :  10 Passed / 5 Failed
+# STATUS :  13 Passed / 2 Failed
 
 def annotate(minefield):
     # Function body starts here
@@ -8,12 +8,25 @@ def annotate(minefield):
     numbers = ""
     same = []
     number = 0
+
+    not_s = [None,None,None,None,None,None,None,None]
+
+
     for item in minefield:
         for i in range(1,len(item)):
             if item[0] == item[i]:
                 same.append(True)
             else:
                 same.append(False)
+    for item in minefield[1:]:
+        if len(item) != len(minefield[0]):
+            raise ValueError("The board is invalid with current input.")
+    for lines in minefield:
+        for item in lines:
+            if item not in [" ","*"]:
+                raise ValueError("The board is invalid with current input.")
+
+
 
     if minefield == []:
         return []
@@ -21,6 +34,7 @@ def annotate(minefield):
         return [""]
     elif all(same) and len(minefield[0]) > 1:
         return minefield
+
     else:
         '''
         loop over each element
@@ -39,9 +53,11 @@ def annotate(minefield):
                         if j < len(minefield[0])-1:
                             if minefield[0][j+1] == "*": #check right
                                 number += 1
+
                         if j > 0:
                             if minefield[0][j-1] == "*": #check left
                                 number += 1
+
                         if 0 < j < len(minefield[0]) - 1:
                             if minefield[0][j - 1] == " " and minefield[0][j + 1] == " ":
                                 number = 99
@@ -49,9 +65,11 @@ def annotate(minefield):
                         if i < len(minefield)-1:
                             if minefield[i+1][0] == "*":
                                 number += 1 # check below
+
                         if i > 0:
                             if minefield[i-1][0] == "*": # check above
                                 number += 1
+
                         if 0 < i < len(minefield) - 1:
                             if minefield[i-1][0] == " " and minefield[i+1][0] == " ":
                                 number = 99
@@ -61,30 +79,45 @@ def annotate(minefield):
 
                             if minefield[i + 1][j] == "*":  # check below
                                 number += 1
+                            else:
+                                not_s[6] = True
                             if j < len(minefield[i])-1:
                                 if minefield[i][j + 1] == "*":  # check right
                                     number += 1
+                                else:
+                                    not_s[4] = True
                                 if minefield[i+1][j+1] == "*": #check bot right
                                     number += 1
+                                else:
+                                    not_s[7] = True
                             if j > 0:
                                 if minefield[i+1][j-1] == "*": #check bot left
                                     number += 1
-
+                                else:
+                                    not_s[5] = True
                         if i > 0:
                             if minefield[i - 1][j] == "*":  # check above
                                 number += 1
+                            else:
+                                not_s[1] = True
 
                             if j > 0:
                                 if minefield[i-1][j-1] == "*": #check top left
                                     number += 1
+                                else:
+                                    not_s[0] = True
 
                             if j < len(minefield[i])-1:
                                 if minefield[i-1][j+1] == "*": #check top right
                                     number += 1
+                                else:
+                                    not_s[2] = True
 
                         if j > 0:
                             if minefield [i][j-1] == "*": #check left
                                 number += 1
+                            else:
+                                not_s[3] = True
 
 
 
@@ -95,11 +128,28 @@ def annotate(minefield):
                         else:
                             numbers += str(number)
                             number = 0
+                        not_s = [None, None, None, None, None, None, None, None]
                     else:
-                        number = 0
-                        numbers += str(number)
+                        flags = []
+                        for item in not_s:
+                            if item is not None:
+                                flags.append(item)
+                        if len(list(set(flags))) == 1 and list(set(flags))[0] == True:
+                            number = 0
+                            numbers += "C"
+                            flags = []
+                        else:
+                            number = 0
+                            numbers += str(number)
+
+                        not_s = [None, None, None, None, None, None, None, None]
+
+                # elif minefield[i][j] == "*":
+                #     numbers += "0"
                 else:
                     numbers += "0"
+
+
             if i == len(minefield)-1:
                 final_str += numbers
             else:
@@ -110,7 +160,7 @@ def annotate(minefield):
 
             #final_field.append(numbers)
             numbers = ""
-
+    xhyz ="STOP"
 
     final_str = final_str.replace("0","*")
     if len(minefield) == 1:
@@ -120,5 +170,7 @@ def annotate(minefield):
             final_field.append(item)
     else:
         final_field = final_str.split()
-
+        for item in range(len(final_field)):
+            if "C" in final_field[item]:
+                final_field[item] = final_field[item].replace("C"," ")
     return final_field
