9bf85b2cefe99c6c8b9efced4232871288743f57
Jacob <58263528+mykasero@users.noreply.github.com>
2024-05-06 22:59:18 +0200

    Add files via upload

--- Chapters/Loops/change.py
+++ Chapters/Loops/change.py
@@ -1,41 +1,12 @@
-# STATUS 8 PASSED / 13
+from itertools import combinations_with_replacement
 def find_fewest_coins(coins, target):
-    coins_change = []
-    coins = coins[::-1]
-
-    ctr = 0
-    one_loop = False
-
     if target < 0:
         raise ValueError("target can't be negative")
-    elif target == 0:
-        return []
-    else:
-        change = [target % coins[i] == 0 for i in range(len(coins))]
+    max_coins = 1 + target // min(coins)
 
-        if len(list(set(change))) == 1 and list(set(change))[0] == False:
-            raise ValueError("can't make target with given coins")
-
-        else:
-            for coin in coins:
-                if target - coin == 0:
-                    return [coin]
-
-            while target > 0:
-                if target % coins[ctr] == 0 and not one_loop:
-                    coins_change.append(coins[ctr])
-                    target -= coins[ctr]
-
-                # elif target - coins[ctr] >= 0 and one_loop:
-                #     coins_change.append(coins[ctr])
-                #     target -= coins[ctr]
-
-                else:
-                    ctr += 1
-
-                if ctr == len(coins):
-                    ctr = 0
-                    one_loop = True
-
-
-    return sorted(coins_change)
+    for i in range(max_coins):
+        combos = combinations_with_replacement(coins, i)
+        for combo in combos:
+            if sum(combo) == target:
+                return sorted(combo)
+    raise ValueError("can't make target with given coins")
