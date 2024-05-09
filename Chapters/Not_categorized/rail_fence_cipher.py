fc3617b7bdc50d5ad06c455b385557a23dcfa211
Jacob <58263528+mykasero@users.noreply.github.com>
2024-05-09 22:25:55 +0200

    Add files via upload

--- Chapters/Not_categorized/rail_fence_cipher.py
+++ Chapters/Not_categorized/rail_fence_cipher.py
@@ -1,36 +1,46 @@
-# Encode tests passed / Decode to-do
+from itertools import cycle
+
 def encode(message, rails):
     encoded = []
     final = ""
+    movement = []
 
     for rail in range(rails):
         encoded.append("")
 
     if rails == 2:
-        for i in range(len(message)):
-            if i%2 == 0:
-                encoded[0] += message[i]
+        movement = [0,1]
+        cycles = cycle(movement)
+
+        for item in cycles:
+            if len(message) > 0:
+                encoded[item] += message[0]
+                message = message[1:]
             else:
-                encoded[1] += message[i]
+                break
+
     elif rails == 3:
-        i_top_bot = 4
-        for i in range(len(message)):
-            if i % 4 == 0:
-                encoded[0] += message[i]
-            elif i % 4 == 2:
-                encoded[2] += message[i]
+        movement = [0,1,2,1]
+        cycles = cycle(movement)
+
+        for item in cycles:
+            if len(message) > 0:
+                encoded[item] += message[0]
+                message = message[1:]
             else:
-                encoded[1] += message[i]
+                break
+
+
     elif rails == 4:
-        for i in range(len(message)):
-            if i % 6 == 0:
-                encoded[0] += message[i]
-            elif i % 6 == 3:
-                encoded [3] += message[i]
-            elif i % 4 == 1 or i % 4 == 3:
-                encoded[1] += message[i]
+        movement = [0,1,2,3,2,1]
+        cycles = cycle(movement)
+
+        for item in cycles:
+            if len(message) > 0:
+                encoded[item] += message[0]
+                message = message[1:]
             else:
-                encoded[2] += message[i]
+                break
 
     for item in encoded:
         final += item
@@ -39,4 +49,73 @@ def encode(message, rails):
 
 
 def decode(encoded_message, rails):
-    pass
+    split_text = []
+    final = ""
+
+    movement = []
+
+
+    if rails == 3:
+        movement = [0, 1, 2, 1]
+        for i in range(rails):
+            split_text.append("")
+
+        for i in range(len(encoded_message)):
+            if i < rails*2:
+                split_text[0] += encoded_message[i]
+
+            elif rails*2 <= i < (rails*2) + (rails*4)-1:
+                split_text[1] += encoded_message[i]
+            else:
+                split_text[2] += encoded_message[i]
+
+    elif rails == 5:
+        movement = [0, 1, 2, 3, 4, 3, 2, 1]
+        for i in range(rails):
+            split_text.append("")
+
+        for i in range(len(encoded_message)):
+            if i < rails-2:
+                split_text[0] += encoded_message[i]
+            elif rails-2 <= i < rails+2:
+                split_text[1] += encoded_message[i]
+            elif rails+2 <= i < (2*rails)+1:
+                split_text[2] += encoded_message[i]
+            elif (2*rails)+1 <= i < (3*rails):
+                split_text[3] += encoded_message[i]
+            else:
+                split_text[4] += encoded_message[i]
+
+    elif rails == 6:
+        movement = [0,1,2,3,4,5,4,3,2,1]
+
+        for i in range(rails):
+            split_text.append("")
+
+        for i in range(len(encoded_message)):
+            if i < rails:
+                split_text[0] += encoded_message[i]
+
+            elif rails <= i < (rails) + (2*rails):
+                split_text[1] += encoded_message[i]
+
+            elif (rails+2) + (2*rails)-2 <= i < rails + 2*((2*rails)):
+                split_text[2] += encoded_message[i]
+
+            elif rails + 2*((2*rails)) <= i < rails + 3*((2*rails)):
+                split_text[3] += encoded_message[i]
+
+            elif rails + 3*((2*rails)) <= i < rails + 4*((2*rails)):
+                split_text[4] += encoded_message[i]
+
+            else:
+                split_text[5] += encoded_message[i]
+
+
+    while len(encoded_message) > len(final):
+        for item in movement:
+            if len(split_text[item]) > 0:
+                final += split_text[item][0]
+                split_text[item] = split_text[item][1:]
+
+    return final
