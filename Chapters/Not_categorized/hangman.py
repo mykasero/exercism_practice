4d58eddce9081eb8f88dfc6a978f2b48adfaef53
Jacob <58263528+mykasero@users.noreply.github.com>
2024-05-28 22:37:55 +0200

    Update hangman.py

--- Chapters/Not_categorized/hangman.py
+++ Chapters/Not_categorized/hangman.py
@@ -42,4 +42,21 @@ class Hangman:
             self.status = STATUS_LOSE
 
         return self.status
+'''
+Instructions
+Implement the logic of the hangman game using functional reactive programming.
 
+Hangman is a simple word guessing game.
+
+Functional Reactive Programming is a way to write interactive programs. 
+It differs from the usual perspective in that instead of saying "when the button is pressed increment the counter", 
+you write "the value of the counter is the sum of the number of times the button is pressed."
+
+Implement the basic logic behind hangman using functional reactive programming. 
+You'll need to install an FRP library for this, this will be described in the language/track specific files of the exercise.
+
+!!!
+Python Special Instructions
+A third party library is not required for this exercise. Please ignore the instructions regarding FRP library.
+'''
+# link to test cases not available
