825457654c614b35755128456c1ce73395018c15
Jacob <58263528+mykasero@users.noreply.github.com>
2024-06-02 20:50:22 +0200

    Update go_counting.py

--- Chapters/Not_categorized/go_counting.py
+++ Chapters/Not_categorized/go_counting.py
@@ -1,6 +1,9 @@
-BLACK = "B"
 WHITE = "W"
-NONE = ""
+BLACK = "B"
+NONE = " "
+UNKNOWN = "?"
+
+
 class Board:
     """Count territories of each player in a Go game
 
@@ -9,9 +12,24 @@ class Board:
     """
 
     def __init__(self, board):
-        self.x_dim = len(board)
-        self.y_dim = len(board[0])
         self.board = board
+        self.rows = len(board)
+        self.cols = 0 if self.rows == 0 else len(board[0])
+
+    def onboard(self, x, y):
+        return 0 <= x < self.cols and 0 <= y < self.rows
+
+    def neighbors(self, x, y):
+        nbr = []
+        for mov_x in (-1, 1):
+            if self.onboard(x + mov_x, y):
+                nbr.append((x + mov_x, y))
+        for mov_y in (-1, 1):
+            if self.onboard(x, y + mov_y):
+                nbr.append((x, y + mov_y))
+
+        return nbr
+
     def territory(self, x, y):
         """Find the owner and the territories given a coordinate on
            the board
@@ -26,12 +44,33 @@ class Board:
                         second being a set of coordinates, representing
                         the owner's territories.
         """
-        stone = self.board[y][x]
+        if not self.onboard(x, y):
+            raise ValueError("Invalid coordinate")
 
+        if self.board[y][x] != NONE:
+            return NONE, set()
 
+        owner = UNKNOWN
+        visited = set()
+        enclosed = set()
+        stack = [(x, y)]
 
-        STOP = "TEST"
+        while len(stack) != 0:
+            c, r = stack.pop()
+            visited.add((c, r))
+            stone = self.board[r][c]
+            if stone == NONE:
+                enclosed.add((c, r))
+                for n in self.neighbors(c, r):
+                    if n not in visited:
+                        stack.append(n)
+            elif stone != owner:
+                owner = stone if owner == UNKNOWN else NONE
 
+        if owner == UNKNOWN:
+            owner = NONE
+
+        return owner, enclosed
 
     def territories(self):
         """Find the owners and the territories of the whole board
@@ -44,4 +83,19 @@ class Board:
                         , i.e. "W", "B", "".  The value being a set
                         of coordinates owned by the owner.
         """
-        pass
+        t = {WHITE: set(), BLACK: set(), NONE: set()}
+
+        vacant = set()
+
+        for c in range(self.cols):
+            for r in range(self.rows):
+                if self.board[r][c] == NONE:
+                    vacant.add((c,r))
+
+        while len(vacant) != 0:
+            v = vacant.pop()
+            who, where = self.territory(v[0], v[1])
+            vacant -= where
+            t[who] |= where
+
+        return t
