c18b58100517018b7d2df33ae1e916b45980c69e
Jacob <58263528+mykasero@users.noreply.github.com>
2024-06-04 20:24:44 +0200

    Add files via upload

--- Chapters/Not_categorized/forth.py
+++ Chapters/Not_categorized/forth.py
@@ -1,4 +1,4 @@
-#STATUS 29 / 48 PASSED
+#STATUS 46 / 48 PASSED
 
 MATH_OPS = ["+","-","*","/"]
 STACK_OPS = ["dup","drop","swap","over"]
@@ -13,20 +13,65 @@ def evaluate(input_data):
     #     raise StackUnderflowError("Insufficient number of items in stack") #works
     test = type(input_data)
     if len(input_data) > 1:
-        splitted = input_data.split()
+        #version with 46/48 passed
+        for i in range(len(input_data)):
+            input_data[i] = input_data[i].lower()
+        new_instr = input_data[len(input_data)-2].split()
+        new_instr = new_instr[1:len(new_instr)-1]
+        lookup = dict()
+        lookup[new_instr[0]] = new_instr[1:]
+        command = input_data[len(input_data)-1].split()
+
+
+        stack = []
+        for item in command:
+            if item not in lookup.keys():
+                stack.append(int(item))
+            else:
+                if item.lower() == "countup":
+                    for numb in lookup[item]:
+                        stack.append(int(numb))
+                    stack.sort()
+                else:
+                    for value in lookup[item]:
+                        if value.lower() == "dup":
+                            stack.append(stack[len(stack)-1])
+                        elif value.lower() == "+":
+                            x = stack.pop()
+                            stack = [stack[0] + x]
+                        elif value.lower() == "-":
+                            x = stack.pop()
+                            stack = [stack[0] - x]
+                        elif value.lower() == "*":
+                            x = stack.pop()
+                            stack = [stack[0] * x]
+                        elif value.lower() == "-":
+                            x = stack.pop()
+                            stack = [stack[0] / x]
 
     else:
         is_op = set()
         stack = []
+        input_data[0] = input_data[0].lower()
         for item in input_data[0].split():
-            if item not in MATH_OPS and item not in STACK_OPS:
+            if item not in MATH_OPS and item not in STACK_OPS :
                 is_op.add(False)
             else:
                 is_op.add(True)
 
+            if item not in MATH_OPS and item not in STACK_OPS:
+                try:
+                    int(item)
+                except:
+                    if item == ";" or item == ":":
+                        raise ValueError("illegal operation")
+                    else:
+                        raise ValueError("undefined operation")
+
+
 
         if len(is_op) == 1 and list(is_op)[0] == False:
-            return [int(item) for item in input_data[0].split()]
+                return [int(item) for item in input_data[0].split()]
 
         else:
             for item in input_data[0].split():
@@ -38,7 +83,7 @@ def evaluate(input_data):
                     if len(stack) < 2:
                         raise StackUnderflowError("Insufficient number of items in stack")
 
-                    elif "0" in stack:
+                    elif 0 in stack:
                         raise ZeroDivisionError("divide by zero")
 
                     elif item == "+":
@@ -82,7 +127,7 @@ def evaluate(input_data):
                             break
 
                     elif item == "swap":
-                        if len(stack) < 3:
+                        if len(stack) < 2:
                             raise StackUnderflowError("Insufficient number of items in stack")
                         else:
                             for item in stack:
@@ -91,7 +136,7 @@ def evaluate(input_data):
                                 break
 
                     elif item == "over":
-                        if len(stack) < 3:
+                        if len(stack) < 2:
                             raise StackUnderflowError("Insufficient number of items in stack")
                         else:
                             stack.append(stack[len(stack)-2])
