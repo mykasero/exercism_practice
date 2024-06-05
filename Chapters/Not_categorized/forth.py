40c106f04d146c40984b6d5decd48c44902e5ba7
Jacob <58263528+mykasero@users.noreply.github.com>
2024-06-05 20:55:13 +0200

    Add files via upload

--- Chapters/Not_categorized/forth.py
+++ Chapters/Not_categorized/forth.py
@@ -1,147 +1,81 @@
-#STATUS 46 / 48 PASSED
-
-MATH_OPS = ["+","-","*","/"]
-STACK_OPS = ["dup","drop","swap","over"]
-
 class StackUnderflowError(Exception):
-    def __init__(self,message):
+    """Exception raised when Stack is not full.
+       message: explanation of the error.
+    """
+
+    def __init__(self, message):
         self.message = message
 
 
 def evaluate(input_data):
-    # if fill this later:
-    #     raise StackUnderflowError("Insufficient number of items in stack") #works
-    test = type(input_data)
-    if len(input_data) > 1:
-        #version with 46/48 passed
-        for i in range(len(input_data)):
-            input_data[i] = input_data[i].lower()
-        new_instr = input_data[len(input_data)-2].split()
-        new_instr = new_instr[1:len(new_instr)-1]
-        lookup = dict()
-        lookup[new_instr[0]] = new_instr[1:]
-        command = input_data[len(input_data)-1].split()
+    user_defined_dict = {}
+
+    for i in range(len(input_data) - 1):
+        data_list = input_data[i].lower().split()
+
+        if data_list[1].isdigit():
+            raise ValueError("illegal operation")
+
+        user_defined_dict[data_list[1]] = " ".join(data_list[2:-1])
+
+        next_data_list = input_data[i + 1].lower().split()
+
+        if next_data_list[0] == ':':
+            for key, value in user_defined_dict.items():
+                if key in next_data_list[2:]:
+                    temp = " ".join(next_data_list[2:]).replace(key, value)
+                    input_data[i + 1] = " ".join(next_data_list[:2]) + ' ' + temp
+
+    return evaluate_stack(input_data[-1].lower(), user_defined_dict)
 
 
-        stack = []
-        for item in command:
-            if item not in lookup.keys():
-                stack.append(int(item))
-            else:
-                if item.lower() == "countup":
-                    for numb in lookup[item]:
-                        stack.append(int(numb))
-                    stack.sort()
-                else:
-                    for value in lookup[item]:
-                        if value.lower() == "dup":
-                            stack.append(stack[len(stack)-1])
-                        elif value.lower() == "+":
-                            x = stack.pop()
-                            stack = [stack[0] + x]
-                        elif value.lower() == "-":
-                            x = stack.pop()
-                            stack = [stack[0] - x]
-                        elif value.lower() == "*":
-                            x = stack.pop()
-                            stack = [stack[0] * x]
-                        elif value.lower() == "-":
-                            x = stack.pop()
-                            stack = [stack[0] / x]
+def evaluate_stack(stack_string, user_defined_dict):
+    int_list = []
 
-    else:
-        is_op = set()
-        stack = []
-        input_data[0] = input_data[0].lower()
-        for item in input_data[0].split():
-            if item not in MATH_OPS and item not in STACK_OPS :
-                is_op.add(False)
-            else:
-                is_op.add(True)
+    for key, value in user_defined_dict.items():
+        if key in stack_string:
+            stack_string = stack_string.replace(key, value)
 
-            if item not in MATH_OPS and item not in STACK_OPS:
-                try:
-                    int(item)
-                except:
-                    if item == ";" or item == ":":
-                        raise ValueError("illegal operation")
-                    else:
-                        raise ValueError("undefined operation")
+    CHEAT_DICT = {'dup': 1, 'over': 2}
+    stack_list = stack_string.split()
 
+    while stack_list:
+        word = stack_list.pop(0)
 
+        if word.isdigit() or (len(word) > 1 and word[0] == '-' and word[1:].isdigit()):
+            int_list.append(int(word))
 
-        if len(is_op) == 1 and list(is_op)[0] == False:
-                return [int(item) for item in input_data[0].split()]
+        elif word in '+-*':
+            check_for_underflow(int_list, 2)
+            int_list = [eval(f"{int_list[-2]} {word} {int_list[-1]}")]
+
+        elif word == '/':
+            check_for_underflow(int_list, 2)
+            if int_list[-1] == 0:
+                raise ZeroDivisionError("divide by zero")
+            int_list = [int_list[-2] // int_list[-1]]
+
+        elif word in CHEAT_DICT:
+            check_for_underflow(int_list, CHEAT_DICT[word])
+            int_list.append(int_list[-1 * CHEAT_DICT[word]])
+
+        elif word == 'drop':
+            check_for_underflow(int_list, 1)
+            int_list.pop()
+
+        elif word == 'swap':
+            check_for_underflow(int_list, 2)
+            int_list = int_list[:-2] + [int_list[-1]] + [int_list[-2]]
+
+        elif word == ':':
+            raise ValueError("illegal operation")
 
         else:
-            for item in input_data[0].split():
-                if item not in MATH_OPS and item not in STACK_OPS:
-                    stack.append(int(item))
+            raise ValueError("undefined operation")
 
-                elif item in MATH_OPS:
-
-                    if len(stack) < 2:
-                        raise StackUnderflowError("Insufficient number of items in stack")
-
-                    elif 0 in stack:
-                        raise ZeroDivisionError("divide by zero")
-
-                    elif item == "+":
-                        for item1 in stack:
-                            if len(stack) > 1:
-                                x = int(stack.pop())
-                                stack[0] = int(stack[0]) + x
-
-                    elif item == "-":
-                        for item1 in stack:
-                            if len(stack) > 1:
-                                x = int(stack.pop())
-                                stack[0] = int(stack[0]) - x
-
-                    elif item == "*":
-                        for item1 in stack:
-                            if len(stack) > 1:
-                                x = int(stack.pop())
-                                stack[0] = int(stack[0]) * x
-
-                    elif item == "/":
-                        for item1 in stack:
-                            if len(stack) > 1:
-                                x = int(stack.pop())
-                                stack[0] = int(int(stack[0]) / x)
-
-                elif item in STACK_OPS:
-                    if len(stack) < 1:
-                        raise StackUnderflowError("Insufficient number of items in stack")
-
-                    elif item == "dup":
-                        for item1 in stack:
-                            x = stack.pop()
-                            stack.append(int(x))
-                            stack.append(int(x))
-                            break
-
-                    elif item == "drop":
-                        for item in stack:
-                            x = stack.pop()
-                            break
-
-                    elif item == "swap":
-                        if len(stack) < 2:
-                            raise StackUnderflowError("Insufficient number of items in stack")
-                        else:
-                            for item in stack:
-                                x = stack.pop()
-                                stack.insert(len(stack)-1,x)
-                                break
-
-                    elif item == "over":
-                        if len(stack) < 2:
-                            raise StackUnderflowError("Insufficient number of items in stack")
-                        else:
-                            stack.append(stack[len(stack)-2])
+    return int_list
 
 
-    STOP = "STOP"
-
-    return stack
+def check_for_underflow(list_, min_stack_len):
+    if len(list_) < min_stack_len:
+        raise StackUnderflowError("Insufficient number of items in stack")
