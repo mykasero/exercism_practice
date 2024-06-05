086bd0ba039a2f67560baee0fe411ac578754396
Jacob <58263528+mykasero@users.noreply.github.com>
2024-06-05 20:55:48 +0200

    Update forth.py

--- Chapters/Not_categorized/forth.py
+++ Chapters/Not_categorized/forth.py
@@ -79,3 +79,25 @@ def evaluate_stack(stack_string, user_defined_dict):
 def check_for_underflow(list_, min_stack_len):
     if len(list_) < min_stack_len:
         raise StackUnderflowError("Insufficient number of items in stack")
+
+'''
+Instructions
+Implement an evaluator for a very simple subset of Forth.
+
+Forth is a stack-based programming language. Implement a very basic evaluator for a small subset of Forth.
+
+Your evaluator has to support the following words:
+
++, -, *, / (integer arithmetic)
+DUP, DROP, SWAP, OVER (stack manipulation)
+Your evaluator also has to support defining new words using the customary syntax: : word-name definition ;.
+
+To keep things simple the only data type you need to support is signed integers of at least 16 bits size.
+
+You should use the following rules for the syntax: a number is a sequence of one or more (ASCII) digits, a word is a sequence of one or more letters, 
+digits, symbols or punctuation that is not a number. (Forth probably uses slightly different rules, but this is close enough.)
+
+Words are case-insensitive.
+'''
+# These tests are auto-generated with test data from:
+# https://github.com/exercism/problem-specifications/tree/main/exercises/forth/canonical-data.json
