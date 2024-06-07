7c485b8574de81e66dc2aa8d81b09342efb09a6e
Jacob <58263528+mykasero@users.noreply.github.com>
2024-06-07 19:42:50 +0200

    Add files via upload

---
+++ Chapters/Not_categorized/rational_numbers.py
@@ -0,0 +1,64 @@
+class Rational:
+    def __init__(self, numer, denom):
+        self.numer = numer
+        self.denom = denom
+
+    def __eq__(self, other):
+        return self.numer == other.numer and self.denom == other.denom
+
+    def __repr__(self):
+        return f'{self.numer}/{self.denom}'
+
+    def __add__(self, other):
+        x,y = self.denom, other.denom
+        self.numer *= other.denom
+        other.numer *= self.denom
+        self.denom = x * y
+        other.denom = x * y
+        if self.numer + other.numer == 0:
+            return Rational(self.numer+other.numer,1)
+        else:
+            return Rational(self.numer + other.numer, self.denom)
+
+    def __sub__(self, other):
+        x, y = self.denom, other.denom
+        self.numer *= other.denom
+        other.numer *= self.denom
+        self.denom = x * y
+        other.denom = x * y
+        if self.numer - other.numer == 0:
+            return Rational(self.numer - other.numer, 1)
+        else:
+            return Rational(self.numer - other.numer, self.denom)
+
+    def __mul__(self, other):
+        x = (self.numer*self.denom)
+        y = (other.denom*other.numer)
+
+        if x == y:
+            return Rational(1,1)
+        elif x == 0 or y == 0:
+            return Rational(0,1)
+        elif self.denom < other.denom:
+            if self.numer < 0 and other.numer < 0 :
+                return Rational(abs(x/self.denom), abs(y/self.denom))
+            else:
+                return Rational(x / self.denom, y / self.denom)
+        else:
+            if self.numer < 0 and other.numer < 0 :
+                return Rational(abs(y/other.denom), abs(x/other.denom))
+            else:
+                return Rational(y/other.denom, x/other.denom)
+
+
+    def __truediv__(self, other):
+        pass
+
+    def __abs__(self):
+        pass
+
+    def __pow__(self, power):
+        pass
+
+    def __rpow__(self, base):
+        pass
