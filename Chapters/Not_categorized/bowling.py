d8d98cebf69f1473d4b91e7f2e4d859a733aca36
Jacob <58263528+mykasero@users.noreply.github.com>
2024-06-09 23:47:38 +0200

    Add files via upload

--- Chapters/Not_categorized/bowling.py
+++ Chapters/Not_categorized/bowling.py
@@ -1,24 +1,40 @@
+# STATUS 10/31 passed
+
 class BowlingGame:
     def __init__(self):
         self.throws = 0
         self.points = []
         self.frame = []
         self.counter = 0
+        self.total = 0
+        self.spare = False
 
     def roll(self, pins):
         self.counter += 1
-        if pins == 10:
-            self.points([10])
-        else:
-            if len(self.frame) < 2:
-                self.frame.append(pins)
-                if len(self.frame) == 2:
-                    self.points.append(self.frame)
-                    self.frame = []
+        self.frame.append(pins)
+
+        if self.spare == True:
+            self.points[-1] += pins
+            self.spare = False
+
+        if len(self.frame) == 1 and self.frame[0] == 10:
+            self.points.append(10)
+            self.frame = []
+        elif len(self.frame) == 2 and self.frame[0] + self.frame[1] == 10:
+            self.points.append(10)
+            self.frame = []
+            self.spare = True
+        elif len(self.frame) == 2 and self.frame[0] + self.frame[1] != 10:
+            self.points.append(self.frame[0]+self.frame[1])
+            self.frame = []
+
 
         STOP = "TEST"
 
 
     def score(self):
-        pass
+        for item in self.points:
+            self.total += item
+
+        return self.total
 
