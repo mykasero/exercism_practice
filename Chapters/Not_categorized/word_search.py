21e6ea33fb717e71eff63b62204acf97f9083d69
Jacob <58263528+mykasero@users.noreply.github.com>
2024-06-20 22:43:56 +0200

    Update word_search.py

--- Chapters/Not_categorized/word_search.py
+++ Chapters/Not_categorized/word_search.py
@@ -1,79 +1,47 @@
-#STATUS works for left to right 12/24 Passed
-import itertools
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
     def __eq__(self, other):
-        TEST = other
         return self.x == other.x and self.y == other.y
 
 
 class WordSearch:
     def __init__(self, puzzle):
         self.puzzle = puzzle
-        self.word = ""
 
     def search(self, word):
-        #              R      D      L      U      TL     TR     BL     BR
-        DIRECTIONS = [0,1,-1]
+        p1, p2 = None, None
 
-        begin_point = None
-        end_point = None
+        width, height = len(self.puzzle[0]) - 1, len(self.puzzle) - 1
 
-        self.word = word
-        splitted = []
-        part = []
+        directions = ((1, 0), (-1, 0), (0, 1), (0, -1),    # → ← ↓ ↑
+                      (-1, -1), (1, -1), (-1, 1), (1, 1))  # ↖ ↗ ↙ ↘
 
-        for item in self.puzzle:
-            for element in item:
-                part.append(element)
-            splitted.append(part)
-            part = []
+        points = []
+        for x in range(width + 1):
+            for y in range(height + 1):
+                if self.puzzle[y][x] == word[0]:
+                    points.append((x,y))
 
-        move_dir = itertools.combinations_with_replacement(DIRECTIONS, 2)
-        check_word = ""
+        STOP = "STOP"
+        for x, y in points:
+            p1 = Point(x, y)
 
-        row = 0
-        column = 0
-        ctr = 0
-        loops = 0
-        while check_word != self.word:
-            move_dir = itertools.combinations_with_replacement(DIRECTIONS, 2)
-            if splitted[row][column] == self.word[ctr]:
-                if ctr == 0:
-                    begin_point = Point(column, row)
-                    check_word += splitted[row][column]
-                    ctr += 1
+            for direction in directions:
+                line = []
+                x2, y2 = x, y
 
-            for x_move, y_move in move_dir:
-                if -1 < row + x_move < len(splitted) and -1 < column + y_move < len(splitted[0]) and (x_move + y_move != 0):
-                    if splitted[row + x_move][column + y_move] == self.word[ctr]:
-                        check_word += splitted[row + x_move][column + y_move]
-                        row += x_move
-                        column += y_move
-                        if ctr == 0:
-                            begin_point = Point(column, row)
+                while 0 <= x2 <= width and 0 <= y2 <= height:
+                    line.append(self.puzzle[y2][x2])
 
-                        ctr += 1
-                        if len(check_word) == len(self.word):
-                            end_point = Point(column, row)
-                        break
+                    if word == ''.join(line):
+                        p2 = Point(x2, y2)
+                        return p1, p2
 
+                    else:
+                        x2 += direction[0]
+                        y2 += direction[1]
 
-            if loops > 0 and begin_point is None:
-                column += 1
-
-            loops += 1
-
-            if loops == len(splitted) * len(splitted[0]):
-                return None
-            elif loops == len(splitted[0]) and len(check_word) != len(self.word):
-                ctr = 0
-                loops = 0
-                check_word = ""
-                row += 1
-                column = 0
-
-        return begin_point, end_point
+        return None
