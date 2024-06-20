2e8fc02930a58e27c72cdedc2384dd23cf2f4537
Jacob <58263528+mykasero@users.noreply.github.com>
2024-06-20 22:44:19 +0200

    Update word_search.py

--- Chapters/Not_categorized/word_search.py
+++ Chapters/Not_categorized/word_search.py
@@ -45,3 +45,26 @@ class WordSearch:
                         y2 += direction[1]
 
         return None
+
+'''
+Instructions
+In word search puzzles you get a square of letters and have to find specific words in them.
+
+For example:
+
+jefblpepre
+camdcimgtc
+oivokprjsm
+pbwasqroua
+rixilelhrs
+wolcqlirpc
+screeaumgr
+alxhpburyi
+jalaycalmp
+clojurermt
+There are several programming languages hidden in the above square.
+
+Words can be hidden in all kinds of directions: left-to-right, right-to-left, vertical and diagonal.
+
+Given a puzzle and a list of words return the location of the first and last letter of each word.
+'''
