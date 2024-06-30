24f1455ba564ad302adda1a648ce78c5b30e118d
Jacob <58263528+mykasero@users.noreply.github.com>
2024-06-30 21:28:55 +0200

    Update react.py

--- Chapters/Not_categorized/react.py
+++ Chapters/Not_categorized/react.py
@@ -1,3 +1,5 @@
+from functools import partial
+
 class InputCell:
     def __init__(self, initial_value):
         self._callbacks = []
@@ -16,14 +18,31 @@ class InputCell:
 
 class ComputeCell:
     def __init__(self, inputs, compute_function):
-        self.input = inputs
-        self.function = compute_function
-        self.value = 0
-
-
+        self._callbacks = []
+        self._value = None
+        self._inputs = [_input.value for _input in inputs]
+        self._compute_function = compute_function
+        self.value = self._compute_function(self._inputs)
+        for i, _input in enumerate(inputs):
+            _input._callbacks.append(partial(self.update, index=i))
+    def update(self, value, index):
+        self._inputs[index] = value
+        if index == len(self._inputs) - 1:
+            self.value = self._compute_function(self._inputs)
+    @property
+    def value(self):
+        return self._value
+    @value.setter
+    def value(self, value):
+        if self._value != value:
+            for c in self._callbacks:
+                c(value)
+        self._value = value
     def add_callback(self, callback):
-        pass
-
+        self._callbacks.append(callback)
     def remove_callback(self, callback):
-        pass
-    
+        try:
+            self._callbacks.remove(callback)
+        except ValueError:
+            pass
+
