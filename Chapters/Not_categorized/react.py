308a7db9215eb8b02f44b65f0db09a2c33279a6e
Jacob <58263528+mykasero@users.noreply.github.com>
2024-06-30 21:29:46 +0200

    Update react.py

--- Chapters/Not_categorized/react.py
+++ Chapters/Not_categorized/react.py
@@ -12,6 +12,7 @@ class InputCell:
     @value.setter
     def value(self, value):
         self._value = value
+        
         for c in self._callbacks:
             c(self._value)
 
@@ -22,24 +23,33 @@ class ComputeCell:
         self._value = None
         self._inputs = [_input.value for _input in inputs]
         self._compute_function = compute_function
+        
         self.value = self._compute_function(self._inputs)
+       
         for i, _input in enumerate(inputs):
             _input._callbacks.append(partial(self.update, index=i))
+    
     def update(self, value, index):
         self._inputs[index] = value
+       
         if index == len(self._inputs) - 1:
             self.value = self._compute_function(self._inputs)
+    
     @property
     def value(self):
         return self._value
+    
     @value.setter
     def value(self, value):
         if self._value != value:
             for c in self._callbacks:
                 c(value)
+        
         self._value = value
+    
     def add_callback(self, callback):
         self._callbacks.append(callback)
+    
     def remove_callback(self, callback):
         try:
             self._callbacks.remove(callback)
