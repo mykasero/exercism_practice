baff50f859d55493a6910881da66e5bebd7d7b66
Jacob <58263528+mykasero@users.noreply.github.com>
2024-07-12 22:56:34 +0200

    Update and rename Chapters/Not_categorized/paasio.py to Chapters/Not_categorized/paasIO/paasio.py

--- Chapters/Not_categorized/paasIO/paasio.py
+++ Chapters/Not_categorized/paasIO/paasio.py
@@ -132,3 +132,17 @@ class MeteredSocket:
     @property
     def send_ops(self):
         return self._send_calls
+
+
+'''
+Instructions
+Report network IO statistics.
+
+You are writing a PaaS, and you need a way to bill customers based on network and filesystem usage.
+
+Create a wrapper for network connections and files that can report IO statistics. The wrapper must report:
+
+The total number of bytes read/written.
+The total number of read/write operations.
+
+'''
