8b0c448ccafbda44613d816a0b3465ee382f0a38
Jacob <58263528+mykasero@users.noreply.github.com>
2024-07-13 15:50:05 +0200

    Update rest_api.py

--- Chapters/Not_categorized/rest_API/rest_api.py
+++ Chapters/Not_categorized/rest_API/rest_api.py
@@ -1,5 +1,3 @@
-#STATUS 7/9 passed
-
 import json
 class RestAPI:
     def __init__(self, database=None):
@@ -11,21 +9,27 @@ class RestAPI:
     def get(self, url, payload=None):
         if payload is not None:
             self.payload = json.loads(payload)
+
         self.url = url
 
         if self.url == "/users":
             if self.payload is None:
                 return '{"users" : []}'
+
             else:
                 return_val = None
                 val = list(self.payload.values())[0]
+
                 for item in self.db['users']:
                     if item['name'] == val[0]:
                         return_val = {'users' : [item]}
+
                 return json.dumps(return_val)
 
     def post(self, url, payload=None):
-        self.payload = json.loads(payload)
+        if payload is not None:
+            self.payload = json.loads(payload)
+
         self.url = url
 
         if self.url == '/add':
@@ -33,7 +37,7 @@ class RestAPI:
                 return_val = self.template.copy()
                 key,val = list(self.payload.keys()), list(self.payload.values())
                 return_val['name'] = val[0]
-                stop = "stop"
+
                 return json.dumps(return_val)
 
         elif self.url == '/iou':
@@ -41,30 +45,44 @@ class RestAPI:
             borrower = self.payload['borrower']
             amount = self.payload['amount']
 
-
-
             for item in self.db['users']:
 
                 if item['name'] == lender:
                     test1 = [person for person in item['owes'].keys() if person not in [lender, borrower]]
                     item['balance'] += amount
+
                     if item['owes'] != {} and test1 == []:
                         for person in list(item['owes'].keys()):
                             if person == borrower:
                                 item['owes'][person] -= amount
+
+                                if item['owes'][person] < 0:
+                                    item['owed_by'].update({person : abs(item['owes'][person])})
+                                    item['owes'] = {}
+
+                                elif item['owes'][person] == 0:
+                                    item['owed_by'], item['owes'] = {},{}
                     else:
                         item['owed_by'].update({borrower : amount})
+
                 elif item['name'] == borrower:
                     test2 = [person for person in item['owed_by'].keys() if person not in [lender, borrower]]
                     item['balance'] -= amount
+
                     if item['owed_by'] != {} and test2 == []:
                         for person in list(item['owed_by'].keys()):
                             if person == lender:
                                 item['owed_by'][person] -= amount
+
+                                if item['owed_by'][person] < 0:
+                                    item['owes'].update({person : abs(item['owed_by'][person])})
+                                    item['owed_by'] = {}
+
+                                elif item['owed_by'][person] == 0:
+                                    item['owed_by'], item['owes'] = {}, {}
                     else:
                         item['owes'].update({lender : amount})
 
             return_val = {'users' : [item for item in self.db['users'] if item['name'] in [lender,borrower]]}
 
-            stop = "stop"
             return json.dumps(return_val)
